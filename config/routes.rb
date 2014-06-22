4c85d37a17774ffcc97c1d30a9044b945fbfe398
Juliana Crispo <julianacrispo@gmail.com>
2014-06-22 17:15:28 -0400

    welcome and wiki controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  get 'welcome/wikis'
+
+  root 'welcome#index'
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +59,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
