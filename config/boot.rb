6d6e1a230547c2673706bd8abfa32f6fc7b3b7f7
Juliana Crispo <julianacrispo@gmail.com>
2014-06-25 10:59:04 -0400

    stripe integration

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,15 @@
 # Set up gems listed in the Gemfile.
+require 'rubygems'
+
+if File.exists?('.env') # Beyond local, '.env' won't exist
+  env_lines = File.read(".env").split("\n")
+  env_lines.each do |line|
+    key, value = line.split("=")
+    ENV[key] ||= value
+  end
+end
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
 require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
+
