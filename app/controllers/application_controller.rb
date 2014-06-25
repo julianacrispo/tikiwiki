255fb23fc7a3505b1d5bae885f5c188bf1a3cc53
Juliana Crispo <julianacrispo@gmail.com>
2014-06-25 14:11:14 -0400

    redirect users after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    wikis_path
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
+  end  
+
 end
+
