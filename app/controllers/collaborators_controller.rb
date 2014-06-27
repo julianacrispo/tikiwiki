25015fe7b48ced81c195de01d54e4508488aad3c
Juliana Crispo <julianacrispo@gmail.com>
2014-06-26 20:30:12 -0400

    collaborators controller edit

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -6,13 +6,28 @@ class CollaboratorsController < ApplicationController
   def create
      @wiki = @wiki.find(params[:wiki_id])
      @user = @wiki.users.find(params[:user_id])
-     collaborate = wiki.collaborators.build(user: @user)
+     collaborate = wiki.collaborators.build(user: @user, wiki: @wiki)
 
-     if collaborate.save
-      flash[:notice] = "collaborators saved"
-      redirect_to [@wiki]
+    if collaborate.save
+      flash[:notice] = "Collaborators Saved"
+      redirect_to @wiki
+    else 
+      flash[:error] = "Unable to add collaborators. Please try again"
+      redirect_to @wiki
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:wiki_id])
+    @user = @wiki.user.find(params[:user_id])
+    collaborate = wiki.collaborators.find(params[:id])
+
+    if collaborate.destroy
+      flash[:notice] = "Removed Collaborator"
+      redirect_to @wiki
+    else 
+      flash[:error] = "Unable to remove collaborator. Please try again."
+      redirect_to @wiki
+    end
   end
 end
