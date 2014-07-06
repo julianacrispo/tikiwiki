5eef112f2364d453f67c499d8b4f24ff01e52655
Juliana Crispo <julianacrispo@gmail.com>
2014-07-06 12:13:52 -0400

    redis togo

--- config/initializers/redis_setup.rb
+++ config/initializers/redis_setup.rb
@@ -1,1 +1,8 @@
-$redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+
+if Rails.env.production?
+  uri = Uri.parse(ENV["REDISTOGO_URL"])
+  $redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+
+  $redis = Redis.new(:host => "127.0.0.1", :port => 6379)
+end
